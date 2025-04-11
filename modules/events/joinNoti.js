1730e773bfb19324c9269161a746e03caf6c5aa8
RisPTG <hoangduy1452000@gmail.com>
2025-04-11 11:33:42 +0700

    Update joinNoti.js

--- modules/events/joinNoti.js
+++ modules/events/joinNoti.js
@@ -1,58 +1,29 @@
 module.exports.config = {
 	name: "joinNoti",
 	eventType: ["log:subscribe"],
-	version: "1.0.1",
-	credits: "CatalizCS",
-	description: "Thông báo bot hoặc người vào nhóm có random gif/ảnh/video",
+	version: "1.0.4",
+	credits: "Mirai Team; mod qh",
+	description: "Thông báo bot hoặc người vào nhóm",
 	dependencies: {
-		"fs-extra": "",
-		"path": "",
-		"pidusage": ""
+		"fs-extra": ""
 	}
 };
 
-module.exports.onLoad = function () {
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const { join } = global.nodemodule["path"];
-
-	const path = join(__dirname, "cache", "joinGif");
-	if (existsSync(path)) mkdirSync(path, { recursive: true });	
-
-	const path2 = join(__dirname, "cache", "joinGif", "randomgif");
-    if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
-
-    return;
-}
-
-
-module.exports.run = async function({ api, event }) {
+module.exports.run = async function({ api, event, Users }) {
 	const { join } = global.nodemodule["path"];
 	const { threadID } = event;
 	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-		api.changeNickname(`『 ${global.config.PREFIX} 』 ⪼ ${(!global.config.BOTNAME) ? "djt me Hoàn simp Dũng lỏ" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
-		const fs = require("fs");
-        await api.sendMessage("Đang thực hiện kết nối...", event.threadID);
-        await new Promise(resolve => setTimeout(resolve, 2000));
-		return api.sendMessage("", event.threadID, () => api.sendMessage({body:`►Terabyte Connection◄\n_________________________\nNhập ${global.config.PREFIX}menu để xem chi tiết các lệnh \n◆--------------◆`, attachment: fs.createReadStream(__dirname + "/cache/joinMp4/hello.gif")} ,threadID));
+		api.changeNickname(` 『 ${global.config.PREFIX} 』⪼ ${(!global.config.BOTNAME) ? "Made by CatalizCS and SpermLord" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+		return api.sendMessage(` Kết nối thành công \n________________________\n🛶 Prefix của bot là: 「 ${global.config.PREFIX} 」\n________________________\n🌼 Hãy sử dụng văn minh !`, threadID);
 	}
 	else {
 		try {
-			const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
+			const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
 			let { threadName, participantIDs } = await api.getThreadInfo(threadID);
-      const moment = require("moment-timezone");
-  var ngay = moment.tz('Asia/Ho_Chi_Minh').format('D/MM/YYYY');
-  var gio = moment.tz('Asia/Ho_Chi_Minh').format('HH:mm:ss');
-  var thu = moment.tz('Asia/Ho_Chi_Minh').format('dddd');
-  if (thu == 'Sunday') thu = '𝗖𝗵𝘂̉ 𝗡𝗵𝗮̣̂𝘁'
-  if (thu == 'Monday') thu = '𝗧𝗵𝘂̛́ 𝗛𝗮𝗶'
-  if (thu == 'Tuesday') thu = '𝗧𝗵𝘂̛́ 𝗕𝗮'
-  if (thu == 'Wednesday') thu = '𝗧𝗵𝘂̛́ 𝗧𝘂̛'
-  if (thu == "Thursday") thu = '𝗧𝗵𝘂̛́ 𝗡𝗮̆𝗺'
-  if (thu == 'Friday') thu = '𝗧𝗵𝘂̛́ 𝗦𝗮́𝘂'
-  if (thu == 'Saturday') thu = '𝗧𝗵𝘂̛́ 𝗕𝗮̉𝘆'
+
 			const threadData = global.data.threadData.get(parseInt(threadID)) || {};
 			const path = join(__dirname, "cache", "joinGif");
-			const pathGif = join(path, `${threadID}.gif`);
+			const pathGif = join(path, `hi.gif`);
 
 			var mentions = [], nameArray = [], memLength = [], i = 0;
 			
@@ -61,28 +32,25 @@ module.exports.run = async function({ api, event }) {
 				nameArray.push(userName);
 				mentions.push({ tag: userName, id });
 				memLength.push(participantIDs.length - i++);
+
+				if (!global.data.allUserID.includes(id)) {
+					await Users.createData(id, { name: userName, data: {} });
+					global.data.userName.set(id, userName);
+					global.data.allUserID.push(id);
+				}
 			}
 			memLength.sort((a, b) => a - b);
 			
-			(typeof threadData.customJoin == "undefined") ? msg = 'Chào mừng thành viên mới {name}.\n✨Chào mừng bạn đã đến {threadName}.\n{type} hiện là đứa thứ {soThanhVien} được thêm vào đây 🎀\nGia nhập vào hồi {thu} {ngay} {gio} \n✨━━━━━━━━━━━✨' : msg = threadData.customJoin;
+			(typeof threadData.customJoin == "undefined") ? msg = "Chào mừng thành viên mới {name}.\n🌼 Thành viên thứ {soThanhVien} \n💕 Chào mừng {name} đã đến với 「 {threadName} 」\n Chúc các bạn sẽ gắn bó lâu dài với nhau 🙆‍♂️" : msg = threadData.customJoin;
 			msg = msg
 			.replace(/\{name}/g, nameArray.join(', '))
-			.replace(/\{type}/g, (memLength.length > 1) ?  'Các bạn' : 'Bạn')
+			.replace(/\{type}/g, (memLength.length > 1) ?  'các bạn' : 'bạn')
 			.replace(/\{soThanhVien}/g, memLength.join(', '))
-			.replace(/\{threadName}/g, threadName)
-      .replace(/\{thu}/g, thu)
-      .replace(/\{ngay}/g, ngay)
-      .replace(/\{gio}/g, gio);
+			.replace(/\{threadName}/g, threadName);
 
 			if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-			const randomPath = readdirSync(join(__dirname, "cache", "joinGif", "randomgif"));
-
 			if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif), mentions }
-			else if (randomPath.length != 0) {
-				const pathRandom = join(__dirname, "cache", "joinGif", "randomgif", `${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
-				formPush = { body: msg, attachment: createReadStream(pathRandom), mentions }
-			}
 			else formPush = { body: msg, mentions }
 
 			return api.sendMessage(formPush, threadID);
